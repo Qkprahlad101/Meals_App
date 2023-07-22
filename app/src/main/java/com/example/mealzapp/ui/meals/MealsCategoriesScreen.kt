f8a165717907cd590966573fa6b11cbda505840d
Qkprahlad101 <k.prahlad101@gmail.com>
2023-07-23 02:48:47 +0530

    Added expandable text feature in meal description.

--- app/src/main/java/com/example/mealzapp/ui/meals/MealsCategoriesScreen.kt
+++ app/src/main/java/com/example/mealzapp/ui/meals/MealsCategoriesScreen.kt
@@ -1,6 +1,6 @@
 package com.example.mealzapp.ui.meals
 
-import android.view.RoundedCorner
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
@@ -10,12 +10,23 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -36,6 +47,7 @@ fun MealsCategoriesScreen() {
 
 @Composable
 fun MealCategory(meal: MealsResponse) {
+    var isExpanded by remember{ mutableStateOf(false) }
     Card(
         shape = RoundedCornerShape(8.dp),
         modifier = Modifier
@@ -49,17 +61,35 @@ fun MealCategory(meal: MealsResponse) {
                 modifier = Modifier
                     .size(72.dp)
                     .padding(4.dp)
+                    .align(Alignment.CenterVertically)
             )
             Column(
                 modifier = Modifier
                     .align(Alignment.CenterVertically)
+                    .fillMaxWidth(0.8f)
                     .padding(16.dp)
             ) {
                 Text(
                     text = meal.name,
                     style = MaterialTheme.typography.headlineMedium
                 )
+                Text(
+                    text = meal.description,
+                    textAlign = TextAlign.Start,
+                    style = MaterialTheme.typography.bodySmall,
+                    overflow = TextOverflow.Ellipsis,
+                    color = Color.White.copy(alpha = 0.3f),
+                    maxLines = if(isExpanded) 10 else 4
+                )
             }
+
+            Icon(
+                imageVector = if(isExpanded) Icons.Filled.KeyboardArrowUp else Icons.Filled.KeyboardArrowDown,
+                contentDescription = "Expand Icon",
+                modifier = Modifier.padding(8.dp)
+                           .align(Alignment.CenterVertically)
+                           .clickable { isExpanded = !isExpanded }
+            )
         }
     }
 }
